626f58e76858ce25217997602086039ff7d6ddc6
HoliestCow <cbritt2@vols.utk.edu>
2017-10-03 08:38:19 -0700

    I Think this works bueno?

--- fileio/root_io.py
+++ fileio/root_io.py
@@ -8,6 +8,4 @@ class GeantImport(object):
         ROOT.gInterpreter.Declare(
             '#include "/home/holiestcow/Documents/2017_fall/ne697_hayward/ddli-code/geant4/include/detevent.hpp"')
         f = rootpy.io.File(filename, 'READ')
-        tree = f.Events
-        test = root_numpy.tree2array(tree)
         return test
